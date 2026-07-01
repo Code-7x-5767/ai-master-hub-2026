51757fc05b5508ddc68157d551e358ccf5a83adf
Code-7x-5767 <ummomomin20@gmail.com>
2026-07-01 22:01:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -209,6 +209,30 @@ behavior:"smooth"
 });
 
 }
+function updateClock(){
+
+const now=new Date();
+
+let h=String(now.getHours()).padStart(2,'0');
+
+let m=String(now.getMinutes()).padStart(2,'0');
+
+let s=String(now.getSeconds()).padStart(2,'0');
+
+const clock=document.getElementById("liveClock");
+
+if(clock){
+
+clock.innerHTML=h+":"+m+":"+s;
+
+}
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
+
 
 
 
